e14c1784ebcfd505a5253a08b9db3446275a2cc6
aamir-talib <aamir.wbr@nxvt.com>
2015-03-19 21:05:34 +0500

    Added support for larger numbers upto 999 novemcentillion

--- src/com/translator/NumberTranslator.java
+++ src/com/translator/NumberTranslator.java
@@ -10,7 +10,9 @@ public class NumberTranslator {
 
   private static final String[] SMALL = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
   private static final String[] TENS  = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-  private static final String[] LARGE = { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion" };
+  private static final String[] LARGE = { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion" };
+  private static final String[] XLARGE = {"decillion", "vigintillion", "trigintillion", "quadragintillion", "quinquagintillion", "sexagintillion", "septuagintillion", "octogintillion", "nonagintillion", "centillion"};
+  private static final String[] PREXLARGE = {"un", "duo", "tre", "quattuor", "quin", "sex", "septen", "octo", "novem"};
 
   public static String translate(String number) {
     if("0".equals(number)){
@@ -31,20 +33,30 @@ public class NumberTranslator {
   private static String translateBigNumber(BigInteger number){
     String words = null;
     if(number.compareTo(BigInteger.valueOf(999)) > 0){
-      for(int bigIdx=0; number.signum() > 0; bigIdx++){
+      for(int bigIdx=-1; number.signum() > 0; ++bigIdx){
         if (number.remainder(BigInteger.valueOf(1000)).signum() > 0) {
-          try{
-            words = translateHundreds(number.remainder(BigInteger.valueOf(1000)).intValue()) + (bigIdx > 0 ? " " + LARGE[bigIdx-1] : "") + (words!=null? ", " + words : "");
-          }catch(IndexOutOfBoundsException e){
-            return "Too long to translate";
+          String largeName = " ";
+          if(bigIdx < 10){
+            largeName += bigIdx >= 0 ? LARGE[bigIdx] : "";
+          }else{
+            try{
+              largeName += (bigIdx%10 != 0 ? PREXLARGE[bigIdx % 10 - 1] : "") + XLARGE[bigIdx / 10 -1];
+            }catch(IndexOutOfBoundsException e){
+              return "Too long to translate";
+            }
           }
+          words = translateHundreds(number.remainder(BigInteger.valueOf(1000)).intValue()) + largeName + (words!=null? ", " + words : "");
         }
         number = number.divide(BigInteger.valueOf(1000));
       }
     }else{
-      words = translateHundreds(number.intValue());  
+      words = translateHundreds(number.intValue());
     }
     return words;
+  }
+
+  private static String translateIllions(int bigIdx){
+    return XLARGE[bigIdx % 10];
   }
 
   // Range 1-999
